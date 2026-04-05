9a2cbddfb7fc189fe10e104e5f018b02cab2b511
CPE3A-castro-christianjoeffrey <2023101662@ms.bulsu.edu.ph>
2026-04-05 08:55:05 +0800

    Replaced alu_tb to alu4_tb

--- CPE3AG1_GRP2and6_PROJECT/CPE3AG1_GRP2and6_PROJECT.srcs/sim_1/new/alu8_tb.v
+++ CPE3AG1_GRP2and6_PROJECT/CPE3AG1_GRP2and6_PROJECT.srcs/sim_1/new/alu8_tb.v
@@ -1,26 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.04.2026 23:14:30
-// Design Name: 
-// Module Name: alu8_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 1ns
 
-
-module alu8_tb(
-
-    );
+module alu8_tb();
 endmodule
